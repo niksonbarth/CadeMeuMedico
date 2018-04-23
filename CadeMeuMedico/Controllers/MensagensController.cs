775ae0b060c484e3121f74ddc625b8704eb0bee8
nikson <niksonbarth@gmail.com>
2018-04-23 19:19:17 -0300

    first commit

---
+++ CadeMeuMedico/Controllers/MensagensController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CadeMeuMedico.Controllers
+{
+    public class MensagensController : Controller
+    {
+        public ActionResult BomDia()
+        {
+            return Content("Bom dia");
+        }
+
+        public ActionResult BoaTarde()
+        {
+            return Content("Boa tarde");
+        }
+    }
+}
